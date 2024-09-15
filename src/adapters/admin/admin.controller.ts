4e3cdf2e9632cb0072812957e29020084dee7ac4
Boris <72118807+Curis-lab@users.noreply.github.com>
2024-09-15 20:19:58 +0630

    test eslint

--- src/adapters/admin/admin.controller.ts
+++ src/adapters/admin/admin.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from 'express';
 import { inject, injectable } from 'inversify';
-// import { vendorTDO } from "@useCases/admin/admin.dtos";
 import { vendorTDO } from '@useCases/admin/admin.dtos';
 import { admin_types } from '@useCases/utils/jd-const';
 import { IAdminInteractor } from '@useCases/admin/admin.gateway';
@@ -20,6 +19,7 @@ export class AdminController {
     this._interactor.searchVendorById(req.params.id, res);
   }
   onGetVendors(req: Request, res: Response) {
+    req.body;
     this._interactor.viewVendors(res);
   }
   onDeleteVendorById(req: Request, res: Response) {
